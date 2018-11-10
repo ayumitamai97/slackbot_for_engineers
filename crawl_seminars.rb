db23b452aa0d9d51ff0d75080c5bd1ee5117647a
ayumitamai97 <ayumitamai97@gmail.com>
2018-11-10 17:46:39 +0900

    Separate slack notification and json parsing

--- crawl_seminars.rb
+++ crawl_seminars.rb
@@ -1,8 +1,9 @@
 require "json"
 require "net/http"
 require "date"
-require 'clockwork'
-require 'active_support/all'
+require "clockwork"
+require "active_support/all"
+require "pry"
 
 class Slack
   def post(text)
@@ -19,18 +20,19 @@ end
 
 class Seminar
   REGIONS = %w(東京 大阪 福岡)
+  SEARCH_START_POSITIONS = %w(1 101).freeze
   def get_connpass_info
     REGIONS.each do |region|
       slack = Slack.new
       slack.post("#{region}で1週間以内に開催される、人気(残席2割未満)のイベントをお知らせします :full_moon_with_face:")
 
-      uris =
-        [URI.parse(URI.encode "https://connpass.com/api/v1/event/?keyword=#{region}&ymd=#{dates}&count=100&start=1"),
-          URI.parse(URI.encode "https://connpass.com/api/v1/event/?keyword=#{region}&ymd=#{dates}&count=100&start=101")]
-  
-      uris.each do |uri|
+      SEARCH_START_POSITIONS.each do |position|
+        encoded_uri =
+          URI.encode("https://connpass.com/api/v1/event/?keyword=#{region}&ymd=#{dates}&count=100&start=#{position}")
+
+        uri = URI.parse(encoded_uri)
         json = JSON.parse Net::HTTP.get_response(uri).body
-        parse_connpass_info(json)
+        notify_slack(json: json, slack: slack)
       end
     end
   end
@@ -40,42 +42,30 @@ class Seminar
   end
 
   private
-  def parse_connpass_info(json)
+  def notify_slack(json:, slack:)
     events = json["events"]
-
     events.each do |event|
-      event_url = event["event_url"]
-      event_title = event["title"]
-      waiting_count = event["waiting"].to_i
-      limit_count = event["limit"].to_i
-      accepted_count = event["accepted"].to_i
-      event_owner = event["series"]["title"] unless event["series"].nil?
-
-      next if waiting_count > 0 || limit_count == 0
-
-      if accepted_count / limit_count > 0.8 && !event_owner.nil?
-        slack.post("*" + event_title + "* by " + event_owner + "\n" + event_url)
-
-      elsif accepted_count / limit_count > 0.8 && event_owner.nil?
-        event_owner = event["owner_display_name"]
-        slack.post("*" + event_title + "* by " + event_owner + "\n" + event_url)
-      end
+      parse_connpass_info(event)
+      next if @waiting_count > 0 || @limit_count == 0
+      slack.post("*" + @event_title + "* by " + @event_owner + "\n" + @event_url)
     end
   end
+
+  def parse_connpass_info(event)
+    @event_url = event["event_url"]
+    @event_title = event["title"]
+    @waiting_count = event["waiting"].to_i
+    @limit_count = event["limit"].to_i
+    @accepted_count = event["accepted"].to_i
+    @event_owner =
+      event["series"] ? event["series"]["title"] : event["owner_display_name"]
+  end
+
   def dates
     today = Date.today
-    dates_arr = []
-
-    for num in 1..7
-      dates_arr << (today + num).to_s.gsub("-","")
-    end
-
-    dates_arr.join(",")
+    (1..7).map{ |day| (today + day).to_s.gsub("-","") }.join(",")
   end
 end
-
-
-
 
 include Clockwork
 
@@ -83,5 +73,4 @@ handler do |job|
   Seminar.new.get_connpass_info
 end
 
-# every(1.day, 'notify_slack.job', :at => '23:00')
-every(1.minute, 'notify_slack.job')
+every(1.day, 'notify_slack.job', :at => '23:00')
