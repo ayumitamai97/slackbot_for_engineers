768dc50b133228439c0bd3e50158cf13eb42e004
ayumitamai97 <ayumitamai97@gmail.com>
2018-11-18 02:37:02 +0900

    Remove unnecessary line

--- clockwork_crawler.rb
+++ clockwork_crawler.rb
@@ -4,5 +4,4 @@ require_relative "seminar"
 
 include Clockwork
 
-every(1.day, 'notify_slack', :at => '23:00') { Seminar.new.get_connpass_info }
-every(1.minute, 'notify_slack') { Seminar.new.get_connpass_info }
+every(1.day, 'notify_slack', :at => '0:00') { Seminar.new.get_connpass_info }
